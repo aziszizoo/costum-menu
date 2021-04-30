7ddb8ce8c8c6b747ee66c5ed3984226ea31d5ccf
azis achamd <zisachmad@gmail.com>
2021-04-30 20:55:46 +0700

    Initial Commit

---
+++ costummenu/costummenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  costummenuApp.swift
+//  costummenu
+//
+//  Created by ZISACHMAD on 30/04/21.
+//
+
+import SwiftUI
+
+@main
+struct costummenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
